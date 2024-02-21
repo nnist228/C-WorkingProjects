22255b915fe1d09b95f7746948d9c4caa40b74ae
Aleksandr Klevtsov main <aleksandrconstructions@tutamail.com>
2024-02-21 17:07:23 +0100

    Build 1.0 HW2 first source code commit

---
+++ HW2/src/move_me_out.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using std::cout;
+#include "move_me_out.h"
+
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
